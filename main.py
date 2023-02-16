7b61cfe47e7a09268a30579d2d5c18824d217f2d
Benjamin Ferrer <125084414+bferrer88@users.noreply.github.com>
2023-02-16 07:34:30 +0000

    Put conversion to float in the input statement for feet of cable

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ print("Welcome to the cable cost program\n")
 
 # Requesting user name and cable amount
 company_name = input("Enter the name of the company: ")
-feet_of_cable = input("\nEnter the cable amount (in feet): ")
+feet_of_cable = float(input("\nEnter the cable amount (in feet): "))
 
 #Convert string to float
-feet_of_cable = float(feet_of_cable)
+#feet_of_cable = float(feet_of_cable)
 
 #Calculate the cost of cable
 cost_of_cable = feet_of_cable * .87
@@ -19,7 +19,7 @@ if feet_of_cable >= 500:
   cost_of_cable = cost_of_cable * 0.50
 
 
-cost_of_cable = round(cost_of_cable, 2)
+# cost_of_cable = round(cost_of_cable, 2)
  
 #Display name of the company and cost of fiber optic cable
 print(f"\nThe name of the company is {company_name}")
